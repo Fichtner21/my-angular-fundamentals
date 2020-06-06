ed11d8f6c2009f7632a033954122d1613a440f22
Fichtner21 <ernest.fichtner@gmail.com>
2020-06-06 10:05:37 +0200

    Dodany moduł auth

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [{ 
+    path: '', 
+    component: AuthComponent,
+    children: [
+      {
+        path: 'register',
+        // 1.
+        /* loadChildren: () => import('./register/register.module') */
+        /*   .then(m => m.RegisterModule), */
+
+        // 2.
+         loadChildren: () => import('./register/register.module') 
+           .then((importedModule) => importedModule.RegisterModule) 
+
+        // 3.
+        // loadChildren: () => {
+        //   const importedModulePromise = import('./register/register.module');
+        //   return importedModulePromise
+        //     .then((importedModule) => importedModule.RegisterModule);
+        // }
+      }
+    ]
+  }, 
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
