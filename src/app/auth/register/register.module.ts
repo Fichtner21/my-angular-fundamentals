a2518c437e1cfbc82474e76e9350c79451ea62d3
Fichtner21 <ernest.fichtner@gmail.com>
2020-06-06 09:56:48 +0200

    Dodanie modułu auth i auth/register

---
+++ src/app/auth/register/register.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+import { RouterModule } from '@angular/router'; //samo sie doda, jak dodamy RouterModule w importach poniżej (@NgModule -> imports [])
+
+
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    CommonModule, 
+    RouterModule.forChild([{
+      path: '', //scieżka do tego komponentu czyli auth/register/
+      component: RegisterComponent //tutaj ładujemy komponent jaki ma być wyświetlony
+    }])   
+  ],
+})
+export class RegisterModule { }
