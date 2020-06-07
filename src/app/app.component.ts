f6f71cb2c5cdc278f12e55cd3965e501643da462
Fichtner21 <ernest.fichtner@gmail.com>
2020-06-07 13:40:17 +0200

    Dodany Title service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { PageTitleService } from './page-title.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +14,18 @@ export class AppComponent implements OnInit{
 
   public isUserLogged$;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router, private pageTitleService: PageTitleService){}
 
   public ngOnInit(){
-    this.isUserLogged$ = this.authService.isUserLogged$;    
+    this.isUserLogged$ = this.authService.isUserLogged$; 
+
+    const routerEvents = this.router.events;
+    
+    routerEvents.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.pageTitleService.setTitle(event.url);
+    });
   }
 
   public logout(){
